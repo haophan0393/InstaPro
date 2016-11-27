a6d8f71a97d55cf3082ab2b618a982b53abe7dca
Hoang Long Dinh <hoanglong0930@gmail.com>
2016-11-28 01:24:30 +0200

    full-img

--- Js/Directives/feedsterPost.js
+++ Js/Directives/feedsterPost.js
@@ -4,7 +4,7 @@ app.directive('feedsterPost', function($timeout){
     scope: {
       post: '='
     },
-    template: ' <img class="avatar" ng-src="{{ post.author.avatar }}"  ><div class="author-name"> {{ post.author.name }}  </div> <img class="like-img" src="Img/like-btn.jpg" ng-show="IsShow == true" > <p class="comment-text">  {{ post.comment.text }} </p><img class="comment-img" ng-src="{{ post.comment.img }}" ng-dblclick="increase(); toggle()" > ',
+    template: ' <img class="avatar" ng-src="{{ post.author.avatar }}"  ><div class="author-name"> {{ post.author.name }}  </div> <img class="like-img" src="Img/like-btn.jpg" ng-show="IsShow == true" > <p class="comment-text">  {{ post.comment.text }} </p><img class="comment-img" ng-src="{{ post.comment.img }}" ng-dblclick="increase(); toggle()" ><a href="{{ post.comment.img }}" data-lightbox="{{ post.comment.img }}" data-title="{{ post.comment.text }}"> <img src="../InstaPro/Img/fullscreen_icon.png" class="full_img" /> </a> ',
    	link: function(scope,element,attrs) {
    		scope.increase = function() {
    			
